e1ce35a51cffb38ab4678eee2147b88df402e073
chenzy7115 <swczy@126.com>
2025-05-12 02:45:58 +0800

    change app_main_display_init

--- main/display/lvgl_ui.c
+++ main/display/lvgl_ui.c
@@ -17,8 +17,6 @@ void app_main_display_init(void)
     label_tem = lv_label_create(scr);
     label_hum = lv_label_create(scr);
 
-    vTaskDelay(1);
-
     // 配置标签样式
     lv_obj_set_width(label, EXAMPLE_LCD_H_RES);
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
@@ -29,35 +27,21 @@ void app_main_display_init(void)
     lv_obj_set_width(label_hum, EXAMPLE_LCD_H_RES);
     lv_obj_set_style_text_align(label_hum, LV_TEXT_ALIGN_RIGHT, 0);
 
-    vTaskDelay(1);
-
     // 定位标签位置
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 5);
     lv_obj_align(label_tem, LV_ALIGN_RIGHT_MID, -20, -10);
     lv_obj_align(label_hum, LV_ALIGN_RIGHT_MID, -20, 20);
 
-    vTaskDelay(1);
-
     // 设置初始文本
     lv_label_set_text(label, " Indoor Env ");
     lv_label_set_text(label_tem, " -- C "); // 默认值
     lv_label_set_text(label_hum, " -- % ");
-    vTaskDelay(1);
 }
 
 // 更新显示数据（周期性调用）
 void update_display_values(int temp_val, int hum_val)
 {
-    // 温度范围校验（DHT11 有效范围：0-50°C）
-    if (temp_val < 0 || temp_val > 500)
-    { // 注意实际值的放大倍数
-        temp_val = 0;
-    }
-    // 湿度范围校验（0-90%）
-    if (hum_val < 0 || hum_val > 900)
-    {
-        hum_val = 0;
-    }
+
     lv_label_set_text_fmt(label_tem, " %d.%d C ", temp_val / 10, temp_val % 10);
     lv_label_set_text_fmt(label_hum, " %d %% ", hum_val);
 }
